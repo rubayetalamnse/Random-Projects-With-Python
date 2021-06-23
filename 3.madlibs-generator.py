8a5587bac682b99f8d164bc16a52ad71fcb3aa0e
Rubayet Alam <201828008@student.mist.ac.bd>
2021-06-23 20:51:19 +0600

    Madlibs Generator

---
+++ 3.madlibs-generator.py
@@ -0,0 +1,4 @@
+adverb = input("enter a adverb: ")
+conjunction = input("Enter an conjunction: ")
+
+print(f"Nuclear power is the use of nuclear reactions to produce electricity. Nuclear power can be obtained from nuclear fission, nuclear decay {conjunction} nuclear fusion reactions. {adverb}, the vast majority of electricity from nuclear power is produced by nuclear fission of uranium {conjunction} plutonium in nuclear power plants.")
